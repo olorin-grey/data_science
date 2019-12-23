2150f1c4eca6aa14617ec7e5bd30a2ed50215354
Wizard <benjohn13@protonmail.com>
2019-12-23 14:38:59 -0800

    Create aggregate_functions.sql

---
+++ aggregate_functions.sql
@@ -0,0 +1,12 @@
+/* 
+Aggregate Functions
+
+AVG(): 			the average of the values in a column
+COUNT(): 		count the number of rows
+MAX()/MIN(): 	the largest/smallest value
+ROUND(): 		round the values in the column
+SUM(): 			the sum of the values in a column
+
+GROUP BY 		is a clause used with aggregate functions to combine data from one or more columns.
+HAVING 			limit the results of a query based on an aggregate property.
+*/
